10eb8be50199913ed4ac790cdb1ff7b5633dad44
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2025-03-28 16:32:13 -0300

    feat: Add static file serving and result limit to search endpoint in server.py

--- api/server.py
+++ api/server.py
@@ -10,21 +10,44 @@ CORS(app)  # Permite requisições da interface Vue.js
 csv_path = "/Users/guilhermekameoka/Desktop/prova/database/active_insurance_providers/Relatorio_cadop.csv"
 df = pd.read_csv(csv_path, encoding="utf-8", sep=";")
 
+
 # Rota para o index.html
 @app.route("/")
 def index():
     return send_from_directory(os.path.dirname(os.path.abspath(__file__)), "index.html")
 
+
+# Rota para os arquivos estáticos
+@app.route("/static/<path:filename>")
+def static_files(filename):
+    return send_from_directory(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"), filename
+    )
+
+
+# server.py
 @app.route("/search", methods=["GET"])
 def search():
     query = request.args.get("q", "").strip().lower()
+    limit = int(request.args.get("limit", 10))  # Limite de resultados
+
     if not query:
         return jsonify({"error": "Parâmetro de busca vazio"}), 400
 
-    # Filtra os registros que comtém a query (case insensitive)
-    results = df[df.apply(lambda row: row.astype(str).str.contains(query, case=False, na=False).any(), axis=1)]
-    
+    # Filtra os registros que contêm a query (case insensitive)
+    results = df[
+        df.apply(
+            lambda row: row.astype(str).str.contains(query, case=False, na=False).any(),
+            axis=1,
+        )
+    ]
+
+    # Limita o número de resultados
+    results = results.head(limit)
+
+    print("Dados retornados do servidor:", results.to_dict(orient="records"))  # Adicione o log
     return jsonify(results.to_dict(orient="records"))
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)

b5db88266bcfe8ed5afda2e9a937c523c900b8f0
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2025-04-01 19:09:49 -0300

    fix: Update SQL queries to handle null values and adjust date ranges for health expenses analysis

--- database/analytical_queries.sql
+++ database/analytical_queries.sql
@@ -1,10 +1,10 @@
 -- Quais as 10 operadoras com maiores despesas em "EVENTOS/ SINISTROS CONHECIDOS OU
 -- AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR" no último trimestre?
 SELECT reg_ans, 
-       SUM(vl_saldo_final - vl_saldo_inicial) AS total_despesas
+    SUM(COALESCE(vl_saldo_final, 0) - COALESCE(vl_saldo_inicial, 0)) AS total_despesas
 FROM health_expenses
-WHERE descricao = "EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MÉDICO HOSPITALAR"
-  AND data BETWEEN '2024-07-01' AND '2024-09-30'  -- 3º trimestre de 2024
+WHERE descricao = 'EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MÉDICO HOSPITALAR'
+AND data BETWEEN '2024-10-01' AND '2024-12-31'
 GROUP BY reg_ans
 ORDER BY total_despesas DESC
 LIMIT 10;
@@ -12,10 +12,10 @@ LIMIT 10;
 
 -- Quais as 10 operadoras com maiores despesas nessa categoria no último ano?
 SELECT reg_ans, 
-       SUM(vl_saldo_final - vl_saldo_inicial) AS total_despesas
+       SUM(COALESCE(vl_saldo_final, 0) - COALESCE(vl_saldo_inicial, 0)) AS total_despesas
 FROM health_expenses
-WHERE descricao = "EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MÉDICO HOSPITALAR"
-  AND YEAR(data) = 2024  -- ano de 2024
+WHERE descricao = 'EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MÉDICO HOSPITALAR'
+  AND EXTRACT(YEAR FROM data) = 2024  -- Ano de 2024
 GROUP BY reg_ans
 ORDER BY total_despesas DESC
 LIMIT 10;
